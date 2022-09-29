cb1c80e49b2b5c6d40b1ac81e69491b03c3f0385
PortalFan202 <anibaldonoso1@gmail.com>
2022-09-29 10:05:58 +0300

    Created server response

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Hello World!' });
+});
+
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
